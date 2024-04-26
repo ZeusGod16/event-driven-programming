7c752ea7ef5f91698f6c309caf9ad747a3938356
ZeusGod16 <emanorandyvi@gmail.com>
2024-04-26 18:29:31 +0800

    version 1.1

--- registration.php
+++ registration.php
@@ -130,6 +130,53 @@
                                 class="form-control mt-2">
                         </div>
                     </div>
+                <!-- Address -->
+                <?php
+                include './config/database.php';
+                ?> 
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        <hr>
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <label>REGION : <b class="text-danger">*</b></label>
+                        <select name="inp_region" id="inp_region" onchange="display_province(this.value)"  required class="form-control mt-2">
+                            <option value="" disabled selected>-- SELECT REGION --</option>
+                            <?php
+                         $sql = "SELECT * FROM ph_region";
+                         $result = $conn->query($sql);
+                         
+                         if ($result->num_rows > 0) {
+                           while($row = $result->fetch_assoc()) {
+                            ?>
+                            <option value="<?= $row['regCode']?>"><?= $row['regDesc'] ?></option>
+                            <?php
+                            }
+                          
+                         } else {
+                           echo "0 results";
+                         }
+                         $conn->close();   
+                            ?>
+                        </select>
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <label>PROVINCE : <b class="text-danger">*</b></label>
+                        <select name="inp_province" id="inp_province" onchange="display_citymun(this.value)"   required class="form-control mt-2">
+                            <option value="" disabled selected>-- SELECT PROVINCE --</option>
+                        </select>
+                 </div>
+                 <div class="col-md-12 mt-3">
+                        <label>CITY/MUNICIPALITY : <b class="text-danger">*</b></label>
+                        <select name="inp_citymun" id="inp_citymun"  onchange="display_brgy(this.value)" required class="form-control mt-2">
+                            <option value="" disabled selected>-- SELECT CITY/MUNICIPALITY --</option>
+                        </select>
+                 </div>
+                 <div class="col-md-12 mt-3">
+                        <label>BARANGGAY : <b class="text-danger">*</b></label>
+                        <select name="inp_brgy" id="inp_brgy" required class="form-control mt-2">
+                            <option value="" disabled selected>-- SELECT BARANGGAY --</option>
+                        </select>
                 </div>
                 <div class="card-footer">
                     <span style="float: right">
@@ -142,7 +189,50 @@
         </div>
     </div>
 </body>
-
+<script>
+    function display_province(regCode){
+        $.ajax({
+        url: './models/ph-address.php',
+        type: 'POST',
+        data: { 
+            'type': 'region',
+            'post_code': regCode
+         },
+        success: function(response){ 
+            console.log (response)
+            $('#inp_province').html(response);
+        }
+    });
+}
+function display_citymun(provCode){
+        $.ajax({
+        url: './models/ph-address.php',
+        type: 'POST',
+        data: { 
+            'type': 'province',
+            'post_code': provCode
+         },
+        success: function(response){
+        console.log (response)
+            $('#inp_citymun').html(response);
+        }
+    });
+}
+function display_brgy(citymunCode){
+        $.ajax({
+        url: './models/ph-address.php',
+        type: 'POST',
+        data: { 
+            'type': 'citymun',
+            'post_code': citymunCode
+         },
+        success: function(response){
+        console.log (response)
+            $('#inp_brgy').html(response);
+        }
+    });
+}
+</script>
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
     crossorigin="anonymous"></script>
 
